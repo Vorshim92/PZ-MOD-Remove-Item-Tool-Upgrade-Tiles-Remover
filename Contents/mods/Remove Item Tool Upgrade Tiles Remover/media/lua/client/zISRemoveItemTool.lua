88fe2d5a720caac8cf27f7f943cbaff88e04a375
Stefano Scalfari <vorshim92@gmail.com>
2024-08-28 17:06:45 +0200

    first commit

---
+++ Contents/mods/Remove Item Tool Upgrade Tiles Remover/media/lua/client/zISRemoveItemTool.lua
@@ -0,0 +1,119 @@
+AdminContextMenu.onRemoveItemTool = function(playerObj)
+    local ui = ISRemoveItemTool:new(0, 0, 340, 150, playerObj);
+    ui:initialise();
+    ui:addToUIManager();
+end
+
+DebugContextMenu.onRemoveItemTool = function(playerObj)
+	local ui = ISRemoveItemTool:new(0, 0, 340, 150, playerObj);
+	ui:initialise();
+	ui:addToUIManager();
+end
+
+
+local ISRemoveItemTool_initialise_ext = ISRemoveItemTool.initialise
+
+
+function ISRemoveItemTool:initialise(...)
+print("ISRemoveItemTool:initialise()")
+ISRemoveItemTool_initialise_ext (self, ...)
+    self.itemType:addOption("Tiles")
+    -- self.floor = self.player:getZ()
+    self.entry = ISTextEntryBox:new(tostring(self.player:getZ()), 60, 80, 20, getTextManager():getFontHeight(UIFont.Small) + 3 * 2);
+    self.entry.font = UIFont.Small;
+    self.entry:initialise();
+    self.entry:instantiate();
+    self.entry:setOnlyNumbers(true);
+    self:addChild(self.entry);
+end
+
+function ISRemoveItemTool:onSelectedFloor()
+-- Fake function
+end
+
+local ISRemoveItemTool_prerender_ext = ISRemoveItemTool.prerender
+function ISRemoveItemTool:prerender()
+    ISRemoveItemTool_prerender_ext(self)
+    self:drawText("Z-axis", 55, 50, 1.0, 1.0, 1.0, 1.0, UIFont.Medium)
+end
+
+
+
+function ISRemoveItemTool:onClick(button)
+    print("ISRemoveItemTool:onClick()")
+
+    if button.internal == "SELECT" then
+        self.selectEnd = false
+        self.startPos = nil
+        self.endPos = nil
+        self.selectStart = true
+    end
+    if button.internal == "REMOVE" then
+        if self.startPos ~= nil and self.endPos ~= nil then
+            local cell = getCell()
+            local x1 = math.min(self.startPos.x, self.endPos.x)
+            local x2 = math.max(self.startPos.x, self.endPos.x)
+            local y1 = math.min(self.startPos.y, self.endPos.y)
+            local y2 = math.max(self.startPos.y, self.endPos.y)
+            local zPosText = self.entry:getText()
+            local zPos = tonumber(zPosText)  -- Converti il testo in numero
+                if not zPos then
+                    print("Errore: il valore di zPos non è un numero valido.")
+                    return
+                end
+            local itemBuffer = {}
+            for x = x1, x2 do
+                for y = y1, y2 do
+                    local sq = cell:getGridSquare(x, y, zPos)
+                    if sq then
+                        if self.itemType:isSelected(1) then
+                            if sq:getObjects():size()-1 ~= nil then
+                                for i=0, sq:getObjects():size()-1 do
+                                    if instanceof(sq:getObjects():get(i), "IsoWorldInventoryObject") then
+                                        local item = sq:getObjects():get(i)
+                                        table.insert(itemBuffer, { it = item, square = sq })
+                                    end
+                                end
+                            end
+                        elseif self.itemType:isSelected(2) then
+                            for i=0, sq:getStaticMovingObjects():size()-1 do
+                                if instanceof(sq:getStaticMovingObjects():get(i), "IsoDeadBody") then
+                                    local item = sq:getStaticMovingObjects():get(i)
+                                    table.insert(itemBuffer, { it = item, square = sq })
+                                end
+                            end
+                        elseif self.itemType:isSelected(3) then
+                            for i=0, sq:getObjects():size()-1 do
+                                if instanceof(sq:getObjects():get(i), "IsoObject") then
+                                    local item = sq:getObjects():get(i)
+                                    table.insert(itemBuffer, { it = item, square = sq })
+                                end
+                            end
+                        end
+                    end
+                end
+            end
+            
+            for i, itemData in ipairs(itemBuffer) do
+                local sq = itemData.square
+                local item = itemData.it
+                if self.itemType:isSelected(1) then
+                    sq:transmitRemoveItemFromSquare(item);
+                    item:removeFromWorld()
+                    item:removeFromSquare()
+                    item:setSquare(nil)
+                elseif self.itemType:isSelected(2) then
+                    sq:removeCorpse(item, false);
+                elseif self.itemType:isSelected(3) then
+                    local square = item:getSquare();
+                    triggerEvent("OnObjectAboutToBeRemoved", item);
+                    square:transmitRemoveItemFromSquare(item);
+                end
+            end
+        end
+    end
+    if button.internal == "CLOSE" then
+        self:destroy();
+        return;
+    end
+end
